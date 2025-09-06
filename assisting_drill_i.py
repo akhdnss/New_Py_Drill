072012fd9a8ab5ff0c8c7f70d8a944fdb76bce76
akhdnss <akhdnss@MacBook-Air-4.local>
2025-09-06 17:54:02 +0700

    6 sept 2025 17:53

--- assisting_drill_i.py
+++ assisting_drill_i.py
@@ -1,12 +1,14 @@
-adress_book = {"john":{"phone":5555, "email":"@gmail.com"}, 
-               "jane":{"phone":9999, "email": "@yahoo.com"}} 
+all_guests = {
+    'Alice': {'apples': 5, 'pretzels': 12},
+    'Bob': {'sandwiches': 3, 'apples': 2, 'cups': 20},
+    'Carol': {'cups': 12, 'sandwiches': 5, 'plates': 2},
+    'David': {'forks': 10, 'knives': 8, 'plates': 4}
+}
 
-#print(f'initial: {adress_book} ')
-#for x, y in adress_book.items(): 
-    #print(f'x: {x} | y: {y} ')
+# def print_items(guest): 
 
-for x in adress_book.get("john"): 
-    # print(type(x))
-    print(adress_book["john"][x])
-
-
+for guest, items in all_guests.items(): 
+    #print(f'GUEST NAME: {guest}')
+    for item, count in items.items(): 
+        # print(f'item: {item} | count: {count} ')
+        print(f'guest name: {guest} brought {item} with {count} quantity')
