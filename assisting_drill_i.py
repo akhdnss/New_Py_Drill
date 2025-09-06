fc49ecbf93ca0221d16e5b69b87e800317824416
akhdnss <akhdnss@MacBook-Air-4.local>
2025-09-06 16:36:21 +0700

    updated 6 sept 2025 | 16:36

--- assisting_drill_i.py
+++ assisting_drill_i.py
@@ -1,3 +1,5 @@
 student_score = {'Alice': 85, 'Bob': 92, 'Charlie': 78}
 value = student_score.get('Alice')
-print(value)
+value_ii = student_score.get('David')
+print(f'value_i: {value} | value_ii: {value_ii} ')
+
