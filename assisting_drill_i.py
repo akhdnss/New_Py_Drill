8d8d2bb5c31021727d7101a27424ba8f344fc554
akhdnss <erkaenambelass@gmail.com>
2025-09-06 16:23:27 +0700

    initial

---
+++ assisting_drill_i.py
@@ -0,0 +1,3 @@
+student_score = {'Alice': 85, 'Bob': 92, 'Charlie': 78}
+value = student_score.get('Alice')
+print(value)
---
+++ drill_i.py
@@ -0,0 +1,19 @@
+car_details = {""
+"make":"honda",
+"model":"civic",
+"year": "2022"
+}
+
+print(f'car details: {car_details}') # 1. 
+
+print(f'model: {car_details["model"]} ') #2
+
+car_details["color"] = "blue" #3
+
+car_details["year"] = "2023" #4
+
+print(f'updated: {car_details} ')
+
+
+
+
