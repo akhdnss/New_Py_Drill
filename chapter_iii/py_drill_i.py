27af9ce51c2e5a92fd275faab40cd6faae40beb3
akhdnss <erkaenambelass@gmail.com>
2025-09-07 19:17:44 +0700

    Add files via upload

---
+++ chapter_iii/py_drill_i.py
@@ -0,0 +1,64 @@
+# import re
+# Text = 'The cat in the hat.'
+# result = re.findall(r'[e]', Text)
+# print(result)
+
+# import re
+# Text = 'I have 2 dogs and 1 cat.'
+# result = re.findall(r'[0-9]', Text)
+# print(result)
+
+# import re
+# Text = 'user_123 is here!'
+# result = re.findall(r'[a-zA-Z0-9]', Text)
+
+# import re
+# Text = 'First line.\nSecond line.'
+# result = re.findall(r'[^a-zA-Z0-9]',Text)
+
+# import re
+# Text = 'The year is 2025.'
+# result = re.findall(r'\D', Text)
+
+# import re
+# Text = 'user_123 is here!'
+# result = re.findall(r'[^a-zA-Z0-9_]', Text)
+
+# import re
+# Text = 'Hello world!'
+# result = re.findall(r'[^ ]', Text)
+
+# import re
+# Text = 'A quick brown fox.'
+# result = re.findall(r'[ \n]', Text)
+
+# import re
+# Text = 'The quick Brown Fox Jumps.'
+# results = re.findall(r'[A-Z]', Text)
+
+##DRILL II
+# import re
+# Text = 'The alphabet starts with a, b, c, d.'
+# result = re.findall(r'[abc]', Text)
+
+# import re
+# Text = 'hij klmnop'
+# result = re.findall(r'[h-k]', Text)
+
+import re
+Text = '123 abc 456 xyz'
+result = re.findall(r'')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
